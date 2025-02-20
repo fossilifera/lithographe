a465289efa1374b6b65b3805c9c735b0ce31f116
Simon Boivin <fossilifera@gmail.com>
2025-02-20 19:26:54 +0100

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ltg-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
