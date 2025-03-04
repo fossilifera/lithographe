7d3957a85ef55d6a2f118578102b7454468abfb7
Simon Boivin <fossilifera@gmail.com>
2025-03-04 13:32:24 +0100

    Change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,9 @@ import {Routes} from '@angular/router';
 import {InventoryViewComponent} from './components/inventory-view/inventory-view.component';
 import {SettingsViewComponent} from './components/settings-view/settings-view.component';
 
-export const ROUTES_PATHS = {
-  settings: "settings"
-}
-
 export const routes: Routes = [
   {path: '', component: InventoryViewComponent},
-  {path: ROUTES_PATHS.settings, component: SettingsViewComponent},
+  {path: 'settings', component: SettingsViewComponent},
   {path: '*', redirectTo: ''},
 
 ];
--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,10 @@
-import { Component } from '@angular/core';
-import {MenuItem} from 'primeng/api';
-import {Menubar} from 'primeng/menubar';
+import {Component} from '@angular/core';
 import {ButtonDirective} from 'primeng/button';
-import {ROUTES_PATHS} from '../../app.routes';
 import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'ltg-menu-bar',
   imports: [
-    Menubar,
-    Menubar,
     ButtonDirective,
     RouterLink,
   ],
@@ -17,19 +12,4 @@ import {RouterLink} from '@angular/router';
 })
 export class MenuBarComponent {
 
-
-  public items: MenuItem[] =[
-      {
-        label: 'Inventaire',
-        icon: 'pi pi-home',
-        routerLink: '/'
-      },
-    {
-      label: 'Paramètres',
-      icon: 'pi pi-cog',
-      routerLink: '/'+ROUTES_PATHS.settings
-    }
-    ];
-
-  protected readonly ROUTES_PATHS = ROUTES_PATHS;
 }
