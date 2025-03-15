f41846eeadf5a97c965d84487f44149ae0cec534
Simon Boivin <fossilifera@gmail.com>
2025-03-15 17:01:18 +0100

    Import travail du POC sur pdf generator

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'jest';
 import presets from 'jest-preset-angular/presets';
+import structuredClone from '@ungap/structured-clone';
 
 export default {
   ...presets.createCjsPreset(),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globals: {
+    structuredClone: structuredClone,
+  }
 } satisfies Config;
--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,15 +1,25 @@
-import {Component} from '@angular/core';
-import {ButtonDirective} from 'primeng/button';
+import {Component, inject} from '@angular/core';
+import {ButtonDirective, ButtonLabel} from 'primeng/button';
 import {RouterLink} from '@angular/router';
+import {Ripple} from 'primeng/ripple';
+import {PdfGeneratorService} from '../../services/pdf-generator.service';
 
 @Component({
   selector: 'ltg-menu-bar',
   imports: [
     ButtonDirective,
     RouterLink,
+    ButtonLabel,
+    Ripple,
   ],
   templateUrl: './menu-bar.component.html'
 })
 export class MenuBarComponent {
+
+  private pdfGeneratorService: PdfGeneratorService = inject(PdfGeneratorService);
+
+  protected createLabels(): void {
+    this.pdfGeneratorService.generateLabels();
+  }
 
 }
