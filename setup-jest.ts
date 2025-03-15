43f58a0139655ebe491b3163e1227bcd21cec4b0
Simon Boivin <fossilifera@gmail.com>
2025-03-15 16:22:03 +0100

    Jest c'est pourri

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,8 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv();
+
+// Jest does not support ESM (used by csv parse)
+jest.mock('csv-parse/browser/esm/sync', () => ({
+  parse: jest.fn()
+}));
