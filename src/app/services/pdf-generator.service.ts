efca13a1e2855b5ed5fb6ff8d94fd3f6a754daab
Simon Boivin <fossilifera@gmail.com>
2025-03-17 19:39:40 +0100

    Ajout jsPDF

--- src/app/services/pdf-generator.service.ts
+++ src/app/services/pdf-generator.service.ts
@@ -1,6 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {InventoryService} from './inventory.service';
 import {LoggerService} from './logger.service';
+import {jsPDF} from 'jspdf';
+import {Specimen} from '../model/specimen';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,36 @@ export class PdfGeneratorService {
   public generateLabels(): void {
 
     this.logger.info('Generating PDF');
+
+    const initialX: number = 10;
+    const initialY: number = 10;
+    const labelWidht: number = 54;
+    const labelHeight: number = 40;
+
+    this.inventoryService.getSelectedSpecimens().subscribe((specimens: Specimen[]) => {
+
+
+      const doc = new jsPDF({
+        unit: 'mm',
+        format: 'a4'
+      });
+      console.log(doc.getFontList());
+      // First rectangle
+      doc.setLineWidth(1);
+      doc.rect(initialX, initialY, labelWidht, labelHeight);
+      // Second rectangle
+      doc.setLineWidth(0.25);
+      doc.rect(initialX + 2, initialY + 2, 50, 36);
+      console.log(specimens.at(0));
+      const text : string = (specimens.at(0)?.data["genre"]??'')+' '+(specimens.at(0)?.data["espce"]??'');
+      console.log(text);
+      doc.text(text, initialX + (labelWidht / 2), initialY + 5, {
+        align: 'center',
+        baseline: 'top',
+        maxWidth: 50
+      });
+      doc.save("labels.pdf");
+    });
+
   }
 }
