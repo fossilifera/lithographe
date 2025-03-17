1241f99cf7d0fd623535f9576d5168993b5b1f4d
Simon Boivin <fossilifera@gmail.com>
2025-03-17 19:08:41 +0100

    Suppression pdfme

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,7 @@
-import type { Config } from 'jest';
+import type {Config} from 'jest';
 import presets from 'jest-preset-angular/presets';
-import * as structuredClone from '@ungap/structured-clone';
 
 export default {
   ...presets.createCjsPreset(),
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  verbose: true,
-  globals: {
-    structuredClone: structuredClone,
-  }
 } satisfies Config;
--- src/app/services/pdf-generator.service.ts
+++ src/app/services/pdf-generator.service.ts
@@ -1,9 +1,5 @@
 import {inject, Injectable} from '@angular/core';
 import {InventoryService} from './inventory.service';
-import {from, map, Observable, switchMap} from 'rxjs';
-import {Specimen} from '../model/specimen';
-import {BLANK_PDF, Template} from '@pdfme/common';
-import {generate} from '@pdfme/generator';
 import {LoggerService} from './logger.service';
 
 @Injectable({
@@ -18,40 +14,5 @@ export class PdfGeneratorService {
   public generateLabels(): void {
 
     this.logger.info('Generating PDF');
-
-    const template: Template = {
-      basePdf: BLANK_PDF,
-      schemas: [
-        [
-          {
-            name: 'a',
-            type: 'text',
-            position: {x: 0, y: 0},
-            width: 10,
-            height: 10,
-          }
-        ],
-      ],
-    };
-
-    this.calculateInputs().pipe(
-      switchMap(
-        (inputs: Record<string, string>[]) => from(generate({template, inputs})))
-    ).subscribe((pdf: any) => {
-      const blob = new Blob([pdf.buffer], {type: 'application/pdf'});
-      window.open(URL.createObjectURL(blob));
-    });
-  }
-
-  private calculateInputs(): Observable<Record<string, string>[]> {
-    return this.inventoryService.getSelectedSpecimens().pipe(
-      map((specimens: Specimen[]) => {
-        return specimens.map((specimen: Specimen) => this.mapInput(String(specimen.id)));
-      })
-    );
-  }
-
-  private mapInput(specimenId: string) {
-    return {a: specimenId};
   }
 }
