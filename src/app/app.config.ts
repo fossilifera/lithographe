47b46460a3ca75f8a1f58fc746667aa40864cedd
Simon Boivin <fossilifera@gmail.com>
2025-03-23 17:15:30 +0100

    Theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import {providePrimeNG} from 'primeng/config';
 import Nora from '@primeng/themes/aura';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
+import {definePreset} from '@primeng/themes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,7 +21,23 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Nora,
+        preset: definePreset(Nora, {
+          semantic: {
+            primary: {
+              50: '{amber.50}',
+              100: '{amber.100}',
+              200: '{amber.200}',
+              300: '{amber.300}',
+              400: '{amber.400}',
+              500: '{amber.500}',
+              600: '{amber.600}',
+              700: '{amber.700}',
+              800: '{amber.800}',
+              900: '{amber.900}',
+              950: '{amber.950}'
+            }
+          }
+        }),
         options: {
           prefix: 'ltg',
           darkModeSelector: '.ltg-app-dark'
