abbe995e6ed6cbffef950fbac16720b843da014a
Simon Boivin <fossilifera@gmail.com>
2025-05-19 22:02:57 +0200

    Wip suppresion système mapping

--- src/app/components/inventory-side-bar/inventory-side-bar.component.ts
+++ src/app/components/inventory-side-bar/inventory-side-bar.component.ts
@@ -3,18 +3,12 @@ import {Button} from 'primeng/button';
 import {InventoryService} from '../../services/inventory.service';
 import {Card} from 'primeng/card';
 import {toSignal} from '@angular/core/rxjs-interop';
-import {VariablesMapperService} from '../../services/variables-mapper.service';
-import {map} from 'rxjs';
-import {Message} from 'primeng/message';
-import {Tag} from 'primeng/tag';
 
 @Component({
   selector: 'ltg-inventory-side-bar',
   imports: [
     Button,
-    Card,
-    Message,
-    Tag
+    Card
   ],
   templateUrl: './inventory-side-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -22,12 +16,8 @@ import {Tag} from 'primeng/tag';
 export class InventorySideBarComponent {
 
   private inventoryService: InventoryService = inject(InventoryService);
-  private readonly variablesMapperService: VariablesMapperService = inject(VariablesMapperService);
   protected inventoryMetadata = toSignal(this.inventoryService.getMetadata());
   protected creationDate = computed(() => this.inventoryMetadata()?.creationDate?.toLocaleDateString());
-
-  readonly listOfVariablesNotMapped = toSignal(this.variablesMapperService.getUnmappedVariables(), {initialValue: []});
-  readonly isAllVariablesAreMapped = computed(() => this.listOfVariablesNotMapped().length === 0);
 
   newInventory(): void {
     this.inventoryService.triggerImportNewInventory();
--- src/app/components/inventory-table/inventory-table.component.ts
+++ src/app/components/inventory-table/inventory-table.component.ts
@@ -1,14 +1,9 @@
-import {ChangeDetectionStrategy, Component, computed, inject, signal, ViewChild, WritableSignal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {TableModule} from 'primeng/table';
 import {InventoryService} from '../../services/inventory.service';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {Checkbox} from 'primeng/checkbox';
 import {FormsModule} from '@angular/forms';
-import {VariablesMapperService} from '../../services/variables-mapper.service';
-import {Tag} from 'primeng/tag';
-import {Button, ButtonLabel} from 'primeng/button';
-import {Popover} from 'primeng/popover';
-import {ColumnMetadata} from '../../model/column-metadata';
 import {Specimen} from '../../model/specimen';
 
 @Component({
@@ -17,10 +12,6 @@ import {Specimen} from '../../model/specimen';
     TableModule,
     Checkbox,
     FormsModule,
-    Tag,
-    Button,
-    Popover,
-    ButtonLabel
   ],
   templateUrl: './inventory-table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -28,19 +19,11 @@ import {Specimen} from '../../model/specimen';
 export class InventoryTableComponent {
 
   protected readonly inventoryService: InventoryService = inject(InventoryService);
-  private readonly variablesMapperService: VariablesMapperService = inject(VariablesMapperService);
-
-  @ViewChild('op') op!: Popover;
 
   readonly columns = toSignal(this.inventoryService.getColumns(), {initialValue: []});
   readonly specimens: Specimen[] = this.inventoryService.getSpecimens();
   readonly selectedSpecimens = toSignal(this.inventoryService.getSpeciemenSelectedIds(), {initialValue: []});
   readonly isAllSpecimensSelected = computed(() => this.inventoryService.getInventorySize() === this.selectedSpecimens().length);
-  readonly listOfVariables = toSignal(this.variablesMapperService.getAllVariables(), {initialValue: []});
-  readonly columnAssignationMap = toSignal(this.variablesMapperService.getColumnsAssignation(), {initialValue: new Map<string, string>});
-  readonly listOfVariablesNotMapped = toSignal(this.variablesMapperService.getUnmappedVariables(), {initialValue: []});
-
-  protected openMenuColumn: ColumnMetadata | undefined = undefined;
 
   public toggleSelect(id: number) {
     this.inventoryService.toggleSpecimenSelection(id);
@@ -48,35 +31,6 @@ export class InventoryTableComponent {
 
   public toggleAllSpecimens(): void {
     this.inventoryService.toggleAllSpecimen();
-  }
-
-  protected displayVaraiblesMappingOptions(event: any, column: ColumnMetadata) {
-    if (this.openMenuColumn?.position === column.position) {
-      this.hideVaraiblesMappingOptions();
-    } else {
-      this.openMenuColumn = column;
-      this.op.show(event);
-
-      if (this.op.container) {
-        this.op.align();
-      }
-    }
-  }
-
-  protected selectVariable(variable: string) {
-    if (this.openMenuColumn) {
-      if (this.columnAssignationMap().get(this.openMenuColumn?.jsonName ?? '') === variable) {
-        this.variablesMapperService.assignColumnToVariable(variable, undefined);
-      } else {
-        this.variablesMapperService.assignColumnToVariable(variable, this.openMenuColumn.jsonName);
-      }
-      this.hideVaraiblesMappingOptions();
-    }
-  }
-
-  private hideVaraiblesMappingOptions(): void {
-    this.op.hide();
-    this.openMenuColumn = undefined;
   }
 
 }
