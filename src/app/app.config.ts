fa54ad023d70417d54ddf8723e6c087768f49f04
Simon Boivin <fossilifera@gmail.com>
2025-06-08 19:49:46 +0200

    Suppresion service worker + dockerfile pour tester

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
-import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideServiceWorker} from '@angular/service-worker';
 import {providePrimeNG} from 'primeng/config';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient} from '@angular/common/http';
@@ -12,10 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
     provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
